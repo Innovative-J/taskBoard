de64610eee014a5bb9f84a01aa1191b2636c8ac6
Innovative-J <juan.cia05678@gmail.com>
2024-04-04 23:32:21 -0500

    submitting new code

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # taskBoard
+
+
+This repo contain starter code for a task board project that will be draggable and work with API's 
+
+- This starter code is meant to help me understand and pracrice my JS knowledge to help prepare me for real world problem solving.
+- This page will help me understanding the areas in JS i will need to improve.
+
+
+## Table of Contents
+
+A short table of contents to help find the specific areas that contain all the information regarding this project.
+
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Usage
+
+Copy or use the gith hub url either from here from git hub in order to view the website. [Website Url](https://innovative-j.github.io/Blog_Post/)
+
+No instalation required just a simple browser will work.
+
+## credits
+
+Guidance and help from online resourcres such as MBN to help understand JS as i practice my programing.
+
+## License
+
+Please refer to the repo for the MIT License
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,53 +4,103 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-    const randomNumber = Math.floor(Math.random()* 100);
-    const randomString = randomNumber.toString();
-    const uniqueId = randomString;
-    return uniqueId;
+  const timeStamp = new Date().getTime();
+  const randomNumber = Math.floor(Math.random() * 100);
+  const randomString = randomNumber.toString();
+  const uniqueId = `${timeStamp}-${randomString}`;
+  return uniqueId;
 }
 const uniqueId = generateTaskId();
-console.log(uniqueId)
+console.log(uniqueId);
 
+function task() {}
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-const card = document.createElement('div');
-card.classList.add('task-card');
-const cardTitle = document.createElement('h3');
-cardTitle.textContent = title;
-const cardDescription= document.createElement('p');
-cardDescription.textContent = description;
+  const card = document.createElement("div");
+  card.classList.add("task-card");
 
-// appending the created elements to the end of the card
-card.appendChild(cardTitle);
-card.appendChild(cardDescription);
+  const cardTitle = document.createElement("h3");
+  cardTitle.textContent = task.title;
 
-return card;
+  const cardDescription = document.createElement("p");
+  cardDescription.textContent = task.description;
 
+  const cardDueDate = document.createElement("p");
+  cardDueDate.textContent = `Due Date: ${task.dueDate}`;
+
+  // appending the created elements to the end of the card
+  card.appendChild(cardTitle);
+  card.appendChild(cardDescription);
+  card.appendChild(cardDueDate);
+
+  return card;
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-
+  // maybe try jq
+  const taskContainer = document.getElementById('task-container');
+    taskList.forEach(task => { 
+        const card = createTaskCard(task);
+        card.setAttribute('draggable', true); 
+        taskContainer.appendChild(card);
+    });
+    const cards = document.querySelectorAll('.task-card');
+    cards.forEach(card => {
+        card.addEventListener('dragstart', (event) => {
+            event.dataTransfer.setData('text', card.id);
+        });
+    });
 }
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
+function handleAddTask(event) {
+    event.preventDefault();
 
-}
+    const title = document.getElementById('taskTitle').value;
+    const description = document.getElementById('taskDescription').value;
+    const dueDate = document.getElementById('dueDate').value;
 
-// Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
+    const newTask = {
+        id: generateTaskId(),
+        title: title,
+        description: description,
+        dueDate: dueDate
+    };
 
+    taskList.push(newTask);
+
+    localStorage.setItem('tasks', JSON.stringify(taskList));
+
+    renderTaskList();
+
+    document.getElementById('taskForm').reset();
+
+    const modal = new bootstrap.Modal(document.getElementById('formModal'));
+    modal.hide();
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
+  const taskId = ui.draggable.attr("id");
+  const taskIndex = taskList.findIndex((task) => task.id === taskId);
 
+  if (taskIndex !== -1) {
+    taskList[taskIndex].status = newStatus;
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+    renderTaskList();
+  } else {
+    console.error("Task not found.");
+  }
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+  renderTaskList();
+  $('#taskForm').submit(handleAddTask);
+  $('.lane').droppable({
+      drop: handleDrop
+  });
+  $('#dueDate').datepicker();
 });
